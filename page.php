9c1f7cbf8d3fe146689d3357d8b71f9e459cbd60
Konrad Korpik <115625670+KonradKorpik@users.noreply.github.com>
2024-03-19 08:17:57 +0100

    Add files via upload

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php // Template name: WIP ?>
+<?php get_header(); ?>
+    <main id="main" class="" style="padding: 2rem 0;">
+        <div class="container" style="">
+            <div class="row" style="">
+                <div class="col-12" style="">
+					<h2 class="" style="">
+						<?php the_title(); ?>
+					</h2>
+					<?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php get_footer(); ?>
